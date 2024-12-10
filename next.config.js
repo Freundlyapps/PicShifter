58f577e150e6ef324f1ba5d411cfc0cde4a19645
GitiQ <sdxxxxsdx@gmail.com>
2024-12-11 00:47:42 +0100

    updated

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const createMDX = require('@next/mdx')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 }
 
 const withMDX = createMDX({
